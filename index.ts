0636d5ee1ddbd02869313771d8b9c76fea37d78e
mutafow <mutafow@gmail.com>
2021-01-26 00:53:05 +0200

    another approach

--- index.ts
+++ index.ts
@@ -1,59 +1,11 @@
-import {
-    closeStream,
-    createStream,
-    getProgress,
-    saveProgress,
-    writeToFile,
-} from "./fileLogger";
-import puppeteer, { Page } from "puppeteer";
+import fs from "fs";
 
-let page: Page;
+const wordlistText = fs.readFileSync("./wordlist.txt").toString();
 
-(async () => {
-    const browser = await puppeteer.launch();
-    page = await browser.newPage();
-    await page.goto(getProgress());
+const words = wordlistText
+    .split(/\r?\n/)
+    .filter((w) => !w.charAt(0).match(/[А-Я]/))
+    .filter((w) => w.length > 1)
+    .map((w) => (w.includes("/") ? w.split("/")[0] : w));
 
-    await scrape(5000, 1000);
-
-    saveProgress(page.url());
-
-    browser.close();
-})();
-
-const scrape = async (nrOfWords: number, maxFileSize: number) => {
-    for (let i = 0; i < nrOfWords / maxFileSize; i++) {
-        await iterateWords(maxFileSize);
-    }
-};
-
-const iterateWords = async (nrOfWords: number) => {
-    const stream = createStream();
-    for (let i = 0; i < nrOfWords; i++) {
-        const transcription = await getWord();
-        writeToFile(stream, transcription);
-        await navigateNextWord();
-        await page.waitForSelector(".translation");
-    }
-    closeStream(stream);
-};
-
-const getWord = async () => {
-    const word = await page.$eval(".translation", (el) =>
-        el.innerHTML.toString()
-    );
-    const foundPosition = word.indexOf(
-        '<span class="found">Търсената дума е намерена</span>'
-    );
-    const transcription = word
-        .substring(0, foundPosition)
-        .split("<br>")
-        .map((w) => w.trim())
-        .filter((w) => !!w.length);
-    console.log(transcription);
-    return transcription;
-};
-
-const navigateNextWord = async () => {
-    await page.click("#wordsList > a:nth-child(3)");
-};
+fs.writeFileSync("./files/wordlist.txt", words.join("\n"), { flag: "w" });
